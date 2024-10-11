9156ef42c1066d6ae9973c2678746e74174cc5b1
Harsha vardhan <harshavardhan1617@gmail.com>
2024-10-11 17:51:21 +0530

    initial commit

---
+++ sampleData.js
@@ -0,0 +1,38 @@
+const users = [
+  {
+    uid: 1,
+    userName: "chrishesmworth",
+    dob: "1010169000000",
+  },
+  {
+    uid: 2,
+    userName: "christianbale",
+    dob: "976300200000",
+  },
+];
+
+const texts = [
+  {
+    textID: 1,
+    text: "hola soy dora comostas",
+  },
+  {
+    textID: 2,
+    text: "hola soy muchachos espaniol",
+  },
+];
+
+const metaData = [
+  {
+    textID: 1,
+    uid: 1,
+    dateTime: "1728629135000",
+    isNotice: false,
+  },
+  {
+    textID: 1,
+    uid: 1,
+    dateTime: "1728629135000",
+    isNotice: false,
+  },
+];
