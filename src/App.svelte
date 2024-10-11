2c8e4c53a174a13d7ee8e6c71ab19503ae2554ab
Harsha vardhan <harshavardhan1617@gmail.com>
2024-10-11 17:56:19 +0530

    add: notice component

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Notice from "./lib/Notice.svelte";
+
+  const sampleNotice = {
+    text: "hola soy dora",
+    user: "dora",
+    time: "now or never",
+  };
+</script>
+
+<Notice notice={sampleNotice} />
---
+++ src/lib/Notice.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let notice = {
+    text: "hey bro",
+    user: "chris Hemsworth",
+    time: "12/2/24 17:32",
+  };
+</script>
+
+<div>
+  <p>{notice.text}</p>
+  <p>{notice.user}</p>
+  <p>{notice.time}</p>
+</div>
+
+<style>
+  div {
+    height: 130px;
+    min-width: 200px;
+    border-radius: 8px;
+    background-color: #95d5b2;
+    display: inline-block;
+    padding: 12px 18px;
+  }
+</style>
