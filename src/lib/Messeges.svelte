f95f17a3b4247d51542b1c4db116b60cf61094f0
Harsha vardhan <harshavardhan1617@gmail.com>
2024-10-14 19:55:38 +0530

    feat: add messeges component

---
+++ src/lib/Messeges.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let msg = "hello";
+</script>
+
+<div>
+  <p>
+    {msg}
+  </p>
+</div>
+
+<style>
+  div {
+    display: inline-block;
+    min-height: 28px;
+    max-width: 300px;
+    border-radius: 16px;
+    padding: 5px 25px 5px 10px;
+    background-color: #e0efda;
+    margin: 8px 4px;
+  }
+
+  div p {
+    transform: translateY(2px);
+    margin: 0;
+    padding: 0;
+  }
+</style>
